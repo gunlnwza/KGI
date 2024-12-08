9f1c5afe48ee0a9c26b880f2b4cbd7a832df4511
Natachat Teecharcharn <118715036+gunlnwza@users.noreply.github.com>
2024-12-08 10:52:00 +0700

    Add .gitignore

--- MyReadWrite.py
+++ MyReadWrite.py
@@ -5,3 +5,12 @@ from datetime import datetime, time
 
 TEAM_NAME = "003_MooDengJengJing"
 
+competition_api_path = os.makedirs("competition_api", exist_ok=True)
+
+
+
+daily_ticks_path = "Daily_Ticks.csv"
+# daily_ticks_df = pd.read_csv(daily_ticks_path)
+
+initial_investment = 10000000
+
